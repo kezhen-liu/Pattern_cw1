21e17e070ae82771a0437b2e4ecd7f10b3768bf5
gloriagegao <gg813@ic.ac.uk>
2016-11-26 18:01:54 +0000

    Q1_b matlab code

---
+++ Q1_b.m
@@ -0,0 +1,26 @@
+clear;
+
+load('face.mat');
+
+trainSet = zeros(2576, 364);
+testSet = zeros(2576, 156);
+
+testSetIndex = 1;
+trainSetIndex = 1;
+for i=1:520
+    res = rem(i,10);
+    if res > 7 || res == 0
+        testSet(:,testSetIndex)=X(:,i);
+        testSetIndex = testSetIndex + 1;
+    else
+        trainSet(:,trainSetIndex)=X(:,i);
+        trainSetIndex = trainSetIndex + 1;
+    end
+end
+
+S=cov(trainSet);
+[V,D] = eig(S);
+
+
+        
+        
