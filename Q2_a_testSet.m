9676cb5d93258fa77ebe80977d8cc33208a1d674
Kezhen Liu <kl2313@eews304-030.ee.ic.ac.uk>
2016-12-17 20:54:16 +0000

    Updated report

--- Q2_a_testSet.m
+++ Q2_a_testSet.m
@@ -9,9 +9,9 @@ load('Q1_b_DataSet.mat');
 
 
 trainMean = mean(trainSet.').';
-trainSetDiff = zeros(2576,TRAIN_NUM);
-for i = 1:TRAIN_NUM
-    trainSetDiff(:,i) = trainSet(:,i)-trainMean;
+testSetDiff = zeros(2576,TEST_NUM);
+for i = 1:TEST_NUM
+    testSetDiff(:,i) = testSet(:,i)-trainMean;
 end
 
 baseNumIndex = 1;
@@ -20,8 +20,8 @@ reconTrain = zeros(EIGVEC_NUM,4,3); %(bases, different number of bases, img)
 
 for baseNum=[10 50 100 EIGVEC_NUM] 
     imgIndex=1;
-    for i=[1 1+int32(TRAIN_NUM/52) 1+2*int32(TRAIN_NUM/52)] % img number in train set
-        reconTrain(1:baseNum,baseNumIndex,imgIndex) = (trainSetDiff(:,i).' * mEigVec(:,EIGVEC_NUM-baseNum+1:EIGVEC_NUM)).';
+    for i=[1 1+int32(TEST_NUM/52) 1+2*int32(TEST_NUM/52)] % img number in train set
+        reconTrain(1:baseNum,baseNumIndex,imgIndex) = (testSetDiff(:,i).' * mEigVec(:,EIGVEC_NUM-baseNum+1:EIGVEC_NUM)).';
         imgIndex = imgIndex+1;
     end
 
@@ -33,17 +33,18 @@ for baseNum=[10 50 100 EIGVEC_NUM]
     end
     baseNumIndex = baseNumIndex + 1;
 end
+
 % To evaluate on the difference between recon and ref image
 diff=zeros(4,3);
 for i=1:4
     for j=1:3
         diff(i,j)= sum(abs(reconTrainImg(:,i,j)-...
-            trainSet(:,(j-1)*int32(TRAIN_NUM/52)+1)));
+            testSet(:,(j-1)*int32(TEST_NUM/52)+1)));
     end
 end
 
 % To show the plot
-IMG_TO_PLOT=2; %change 1 here
+IMG_TO_PLOT=3; %change 1 here
 Img=zeros(56,46,4);
 A=zeros(56,46);
 basesUsed=[10 50 100 EIGVEC_NUM];
@@ -61,9 +62,8 @@ end
 subplot(1,5,5);
 ref = zeros(56,46);
 for i=1:46
-        ref(:,i)=trainSet(1+(i-1)*56:i*56,(IMG_TO_PLOT-1)*int32(TRAIN_NUM/52)+1); 
+        ref(:,i)=testSet(1+(i-1)*56:i*56,(IMG_TO_PLOT-1)*int32(TEST_NUM/52)+1); 
 end
 Img1 = mat2gray(ref, [min(min(ref)) max(max(ref))]);
 imshow(Img1);
 title('Reference');
-
